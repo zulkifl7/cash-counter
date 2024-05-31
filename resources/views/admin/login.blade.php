ce5683c724bbce4a790269fe451c765296b99004
Zulkifl Ajmal <zulkiflajmal@gmail.com>
2024-05-31 19:29:51 +0530

    Add: admin and user login pages

---
+++ resources/views/admin/login.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Admin Login</title>
+</head>
+
+<body>
+    <h1>Welcome to Admin Login</h1>
+
+
+    <a href="{{ route('home') }}">logout</a>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+}) -> name('home');
+
+
+Route::get('/admin/login', [AdminController::class, 'login']
+)-> name('admin.login');
+
+Route::get('/user/login',[UserController::class,'login']
+)-> name('user.login');
